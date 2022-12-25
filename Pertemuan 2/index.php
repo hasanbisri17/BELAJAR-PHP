24e25fe3faf3e41d2186b87310eee8e5cb6bf343
hasanbisri17 <bisri171998@gmail.com>
2022-12-25 19:27:04 +0700

    penambahan penulisan sintaks PHP di HTML

--- Pertemuan 2/index.php
+++ Pertemuan 2/index.php
@@ -8,8 +8,22 @@
     echo "Mohamad Hasan Bisri <br>";
     echo true;
 
-
-
-
-
+    //penulisan sintaks PHP 
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo, nama saya <?php echo "Mohamad Hasan Bisri"; ?></h1>
+    <?php 
+        echo "h1>Hallo, nama saya Mohamad Hasan Bisri</h1>";
+    ?>
+</body>
+</html>
