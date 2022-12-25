ba788790a725bc50b38991748f186804e41a1380
hasanbisri17 <bisri171998@gmail.com>
2022-12-25 19:21:56 +0700

    penambahan bolean di pertemuan 2

--- Pertemuan 2/index.php
+++ Pertemuan 2/index.php
@@ -1,12 +1,12 @@
 <?php
     //Pertemuan 2 => SIntaks PHP
 
-//Standar Output
-// echo, print
-//print_r
-//var_dump
-echo "Mohamad Hasan Bisri";
-
+    //Standar Output
+    // echo, print
+    //print_r
+    //var_dump
+    echo "Mohamad Hasan Bisri <br>";
+    echo true;
 
 
 
