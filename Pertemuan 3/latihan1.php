f90e75d13b49b86a1ab5b14e41a50f28c9b5a92f
hasanbisri17 <bisri171998@gmail.com>
2022-12-26 10:11:13 +0700

    Materi Pengulangan while di pertemuan 3

--- Pertemuan 3/latihan1.php
+++ Pertemuan 3/latihan1.php
@@ -4,9 +4,16 @@
 
     //Materi Pengulangan For
 
-    for( $i = 0; $i < 5; $i++) {
-        echo "Hello World! <br>";
-    }
+    // for( $i = 0; $i < 5; $i++) {
+    //     echo "Hello World! <br>";
+    // }
+    
+    //Materi Pengulangan while
 
+    $i = 0;
+    while( $i < 5 ) {
+        echo "Hello World! <br>";
+        $i++;
+    }
 
 ?>
