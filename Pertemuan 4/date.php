1561e8ecd0b755cb47936fc039bf55f0206a0328
hasanbisri17 <bisri171998@gmail.com>
2022-12-26 15:11:19 +0700

    pertemuan4 mengenai function

--- Pertemuan 4/date.php
+++ Pertemuan 4/date.php
@@ -1,6 +1,10 @@
 <?php 
     //Date
     //date untuk menampilkan tanggal dengan format tertentu
-    echo date("l, d-M-Y");
+    // echo date("l, d-M-Y");
+
+    //Time
+    // UNIX Timestamp / EPOCH teime
+    echo time();
 
 ?>
