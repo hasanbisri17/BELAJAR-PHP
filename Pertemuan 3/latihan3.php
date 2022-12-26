55ffc8e482464e426815e4ad2b68baf0183de983
hasanbisri17 <bisri171998@gmail.com>
2022-12-26 12:19:10 +0700

    Percabangan menggunakan if

---
+++ Pertemuan 3/latihan3.php
@@ -0,0 +1,17 @@
+<?php 
+// Pengkondisian / Percabangan
+// Ada beberapa macam percangan seperti
+// if else
+// if else if else
+// ternary
+// switch
+
+//Percabangan menggunakan if
+$x = 30;
+if( $x < 20 ){
+    echo "benar";
+}
+
+
+
+?>
