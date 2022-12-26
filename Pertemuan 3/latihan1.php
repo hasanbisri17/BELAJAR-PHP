6d3f03c3330c568ecf91268c622e078df0785c69
hasanbisri17 <bisri171998@gmail.com>
2022-12-26 10:08:39 +0700

    Materi Pengulangan For di pertemuan 3

---
+++ Pertemuan 3/latihan1.php
@@ -0,0 +1,12 @@
+<?php 
+    //Pengulangan
+    // Macam" Pengulangan ada ( For, While, do ... while, foreach [pengulangan khusus array] )
+
+    //Materi Pengulangan For
+
+    for( $i = 0; $i < 5; $i++) {
+        echo "Hello World! <br>";
+    }
+
+
+?>
